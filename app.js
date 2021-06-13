d488b0b18d80b0a65fcdd47584a9b6c49ae77be2
LAgrl <lizethariasc14@gmail.com>
2021-06-13 16:19:27 -0700

    create HTML elements

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const generatePage = require('./src/page-template');
+const generatePage = require('./src/page-template.js');
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -129,10 +129,12 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-  const pageHTML = generatePage(portfolioData);
-    
-    // fs.writeFile('./index.html', pageHTML, err => {
-    //   if (err) throw new Error(err);
-    //   console.log('Page created! Check out index.html in this directory to see it!');
-    // }); 
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
   });
+
